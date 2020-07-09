9c5b963c8f5382aae20b6868fe5a791e0fd3c51f
gnembon <gnembonmc@gmail.com>
2020-07-08 23:02:27 -0300

    20w28a

--- src/main/java/carpet/mixins/WorldChunk_fillUpdatesMixin.java
+++ src/main/java/carpet/mixins/WorldChunk_fillUpdatesMixin.java
@@ -14,11 +14,11 @@ public class WorldChunk_fillUpdatesMixin
 {
     @Redirect(method = "setBlockState", at = @At(
             value = "INVOKE",
-            target ="Lnet/minecraft/block/BlockState;method_26197(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V"
+            target = "Lnet/minecraft/block/BlockState;onBlockAdded(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V"
     ))
     private void onAdded(BlockState blockState, World world_1, BlockPos blockPos_1, BlockState blockState_1, boolean boolean_1)
     {
         if (!CarpetSettings.impendingFillSkipUpdates)
-            blockState.method_26197(world_1, blockPos_1, blockState_1, boolean_1); // onBLockAdded
+            blockState.onBlockAdded(world_1, blockPos_1, blockState_1, boolean_1);
     }
 }
